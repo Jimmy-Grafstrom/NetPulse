329e05d4180bfd622ac8bc962fda988e75fa6e4c
Jimmy-Grafstrom <jimmy.grafstrom@gmail.com>
2026-01-13 20:22:21 +0100

    chore: initial project setup with Spring Boot 4 and Java 25

---
+++ src/main/java/se/jimmy/netpulse/NetPulseApplication.java
@@ -0,0 +1,13 @@
+package se.jimmy.netpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetPulseApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(NetPulseApplication.class, args);
+    }
+
+}
