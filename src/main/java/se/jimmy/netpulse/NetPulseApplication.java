9ff463d0eb70456c3b14d73594f3b327fc1415ff
Jimmy-Grafstrom <jimmy.grafstrom@gmail.com>
2026-01-15 21:56:17 +0100

    feat: implement type-safe mapping from application.yml to record NetPulseProperties.java

--- src/main/java/se/jimmy/netpulse/NetPulseApplication.java
+++ src/main/java/se/jimmy/netpulse/NetPulseApplication.java
@@ -2,8 +2,11 @@ package se.jimmy.netpulse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class NetPulseApplication {
 
     static void main(String[] args) {
---
+++ src/main/java/se/jimmy/netpulse/config/NetPulseProperties.java
@@ -0,0 +1,25 @@
+package se.jimmy.netpulse.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "netpulse")
+public record NetPulseProperties(
+        Monitor monitor,
+        Logging logging
+
+) {
+    public record Monitor (
+            String targetHost,
+            long intervalsMs,
+            Thresholds thresholds
+    ) {}
+
+    public record Thresholds(
+            long warningsMs,
+            long criticalMs
+    ) {}
+
+    public record Logging(
+            String cvsPath
+    ) {}
+}
