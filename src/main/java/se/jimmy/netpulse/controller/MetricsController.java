1d9cee131808c69a698f11f1691d3703193cc997
Jimmy-Grafstrom <jimmy.grafstrom@gmail.com>
2026-01-16 14:16:00 +0100

    feat: implement MetricsController to expose REST endpoints with latest metrics and history

--- src/main/java/se/jimmy/netpulse/controller/MetricsController.java
+++ src/main/java/se/jimmy/netpulse/controller/MetricsController.java
@@ -1,4 +1,28 @@
 package se.jimmy.netpulse.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.jimmy.netpulse.model.NetworkMetric;
+import se.jimmy.netpulse.repository.MetricsRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/metrics")
+@RequiredArgsConstructor
 public class MetricsController {
+
+    private final MetricsRepository repository;
+
+    @GetMapping("/latest")
+    public NetworkMetric getLatest() {
+        return repository.getLatest();
+    }
+
+    @GetMapping("/history")
+    public List<NetworkMetric> getHistory() {
+        return repository.getHistory();
+    }
 }
