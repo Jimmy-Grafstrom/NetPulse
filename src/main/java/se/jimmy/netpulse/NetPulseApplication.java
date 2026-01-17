7d1d679d5a55e4703e8c296d9cc0fd5c4952389d
Jimmy-Grafstrom <jimmy.grafstrom@gmail.com>
2026-01-17 13:00:26 +0100

    fix: set default timezone to Europe/Stockholm

--- src/main/java/se/jimmy/netpulse/NetPulseApplication.java
+++ src/main/java/se/jimmy/netpulse/NetPulseApplication.java
@@ -1,10 +1,13 @@
 package se.jimmy.netpulse;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -15,4 +18,8 @@ public class NetPulseApplication {
         SpringApplication.run(NetPulseApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Stockholm"));
+    }
 }
